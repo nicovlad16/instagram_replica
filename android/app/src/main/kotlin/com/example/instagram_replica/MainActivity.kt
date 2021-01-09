28b1d6668512944d16dfcc8587a2422afe68c819
Nicoleta Vlad <nicovlad16@gmail.com>
2021-01-09 15:11:25 +0200

    Initial commit: create project, add redux

---
+++ android/app/src/main/kotlin/com/example/instagram_replica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instagram_replica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/presentation/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:instagram_replica/src/presentation/home.dart';
+
+
+// ignore: avoid_classes_with_only_static_members
+class AppRoutes {
+  static const String home = '/';
+
+  static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    home: (BuildContext context) {
+      return const Home();
+    },
+  };
+}
