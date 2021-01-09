f1817c851db8c3bb1866e19d6b7b50d1e396d4cc
Nicoleta Vlad <nicovlad16@gmail.com>
2021-01-09 15:38:47 +0200

    add init and future builder

---
+++ lib/src/init/init.dart
@@ -0,0 +1,17 @@
+import 'package:instagram_replica/src/epics/app_epics.dart';
+import 'package:instagram_replica/src/models/index.dart';
+import 'package:instagram_replica/src/reducer/reducer.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_epics/redux_epics.dart';
+
+Future<Store<AppState>> init() async {
+  const AppEpics appEpics = AppEpics();
+
+  return Store<AppState>(
+    reducer,
+    initialState: AppState.initialState(),
+    middleware: <Middleware<AppState>>[
+      EpicMiddleware<AppState>(appEpics.epics)
+    ],
+  );
+}
